bd645adedb815bedcf6df1d9600cadc7c6ba6c12
k1w1-a0style <h91874350@gmail.com>
2025-12-17 18:33:05 +0100

    Add components/MusicPlayer.tsx

---
+++ components/MusicPlayer.tsx
@@ -0,0 +1,18 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+const MusicPlayer = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Music Player</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f0f0f0'
+  }
+});
+
+export default MusicPlayer;
