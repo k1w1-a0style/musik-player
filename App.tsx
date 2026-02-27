58883ceb28f2a3737cdcfb3da6c20557f1904af8
k1w1-a0style <h91874350@gmail.com>
2026-02-28 00:02:11 +0100

    chore: sync: App.tsx

--- App.tsx
+++ App.tsx
@@ -1,42 +1,10 @@
-import React, { useEffect } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { MusicProvider } from './contexts/MusicContext';
-import MusicPlayer from './screens/MusicPlayer';
-import Playlists from './screens/Playlists';
-import Equalizer from './screens/Equalizer';
-import Library from './screens/Library';
-import { registerForPushNotificationsAsync } from './utils/pushNotifications';
+import React from 'react';
+import { View, Text } from 'react-native';
 
-const Stack = createNativeStackNavigator();
-
-const App = () => {
-  useEffect(() => {
-    // Register for push notifications on app start
-    registerForPushNotificationsAsync();
-  }, []);
-
+export default function App(){
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <MusicProvider>
-        <NavigationContainer>
-          <Stack.Navigator
-            initialRouteName="MusicPlayer"
-            screenOptions={{
-              headerShown: false,
-              animation: 'slide_from_right',
-            }}
-          >
-            <Stack.Screen name="MusicPlayer" component={MusicPlayer} />
-            <Stack.Screen name="Library" component={Library} />
-            <Stack.Screen name="Playlists" component={Playlists} />
-            <Stack.Screen name="Equalizer" component={Equalizer} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </MusicProvider>
-    </GestureHandlerRootView>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>APK Builder - New Project</Text>
+    </View>
   );
-};
-
-export default App;
+}
