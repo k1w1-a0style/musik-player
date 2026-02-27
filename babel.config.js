cdeb795531d9b6f3dc99634283d6591f01b4f328
k1w1-a0style <h91874350@gmail.com>
2026-02-28 00:02:15 +0100

    chore: sync: babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,4 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          extensions: ['.ts', '.tsx', '.js', '.json'],
-          alias: {
-            '@': './src',
-            '@theme': './theme',
-          },
-        },
-      ],
-    ],
-  };
+  return { presets: ['babel-preset-expo'] };
 };
