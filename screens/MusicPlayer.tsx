1b2bb4cec3d2af6bdb268622549c69bb3ebfd80d
k1w1-a0style <h91874350@gmail.com>
2026-03-05 00:43:21 +0100

    Add screens/MusicPlayer.tsx

--- screens/MusicPlayer.tsx
+++ screens/MusicPlayer.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { useMusicContext } from '../src/contexts/MusicContext';
+import * as MediaLibrary from 'expo-media-library';
 
 const MusicPlayer: React.FC = () => {
   const { currentSong } = useMusicContext();
 
   return (
     <View>
-      <Text>Now Playing: {currentSong.title}</Text>
+      <Text>Now Playing: {currentSong?.title}</Text>
     </View>
   );
 };
