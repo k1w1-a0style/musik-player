823bc1500017ce67d618a709d1096acdbd5d1f0d
k1w1-a0style <h91874350@gmail.com>
2026-03-05 23:55:04 +0100

    chore: sync: components/MusicPlayer.tsx

--- components/MusicPlayer.tsx
+++ components/MusicPlayer.tsx
@@ -1,6 +1,7 @@
+import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const MusicPlayer = () => {
+const MusicPlayer: React.FC = () => {
   return (
     <View style={styles.container}>
       <Text>Music Player</Text>
