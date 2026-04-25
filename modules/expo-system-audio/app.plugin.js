ebb7d97a33d6eecf16d89ff813414983105a4410
k1w1-a0style <h91874350@gmail.com>
2026-04-25 22:15:22 +0200

    fix: add config plugin entry for expo-system-audio

---
+++ modules/expo-system-audio/app.plugin.js
@@ -0,0 +1,11 @@
+const { createRunOncePlugin } = require('@expo/config-plugins');
+
+function withExpoSystemAudio(config) {
+  return config;
+}
+
+module.exports = createRunOncePlugin(
+  withExpoSystemAudio,
+  'expo-system-audio',
+  '1.0.0',
+);
