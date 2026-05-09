270aeea8a609147d628d0ddbffbe1e66215a2aff
k1w1-a0style <h91874350@gmail.com>
2026-05-09 08:11:24 +0200

    Simplify navigation param list types for maintainability

--- types/navigation.ts
+++ types/navigation.ts
@@ -1,14 +1,13 @@
-import type { APP_STACK_ROUTES, APP_TAB_ROUTES } from './routes';
-
-type ValueOf<T> = T[keyof T];
-
-export type AppStackRouteName = ValueOf<typeof APP_STACK_ROUTES>;
-export type AppTabRouteName = ValueOf<typeof APP_TAB_ROUTES>;
-
-export type AppStackParamList = Record<AppStackRouteName, undefined | { songId: string }> & {
+export type AppStackParamList = {
   MainTabs: undefined;
   NowPlaying: undefined;
   TrackInfo: { songId: string };
 };
 
-export type AppTabParamList = Record<AppTabRouteName, undefined>;
+export type AppTabParamList = {
+  Bibliothek: undefined;
+  Playlists: undefined;
+  Equalizer: undefined;
+  Cover: undefined;
+  ID3: undefined;
+};
