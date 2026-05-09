fb487057bec7fe8400ed807f0f4847ff1e2ac6a8
k1w1-a0style <h91874350@gmail.com>
2026-05-09 07:24:30 +0200

    Tie navigation param list names to route constants

--- types/navigation.ts
+++ types/navigation.ts
@@ -1,13 +1,14 @@
-export type AppStackParamList = {
+import type { APP_STACK_ROUTES, APP_TAB_ROUTES } from './routes';
+
+type ValueOf<T> = T[keyof T];
+
+export type AppStackRouteName = ValueOf<typeof APP_STACK_ROUTES>;
+export type AppTabRouteName = ValueOf<typeof APP_TAB_ROUTES>;
+
+export type AppStackParamList = Record<AppStackRouteName, undefined | { songId: string }> & {
   MainTabs: undefined;
   NowPlaying: undefined;
   TrackInfo: { songId: string };
 };
 
-export type AppTabParamList = {
-  Bibliothek: undefined;
-  Playlists: undefined;
-  Equalizer: undefined;
-  Cover: undefined;
-  ID3: undefined;
-};
+export type AppTabParamList = Record<AppTabRouteName, undefined>;
