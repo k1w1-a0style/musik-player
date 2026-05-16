e58d94ea2de09224cc0f957b0299eca497d13501
k1w1-a0style <h91874350@gmail.com>
2026-05-16 05:53:56 +0200

    refactor: extract library folder messages

---
+++ utils/libraryFolderMessages.ts
@@ -0,0 +1,9 @@
+export const libraryFolderMessages = {
+  unsupportedTitle: 'Nicht unterstützt',
+  folderPickerUnsupportedMessage: 'Die Ordnerauswahl wird aktuell nur unter Android unterstützt.',
+  folderPickerUnavailableMessage: 'Die Ordnerauswahl ist auf diesem Gerät nicht verfügbar. Nutze stattdessen den normalen Import.',
+  cancelledTitle: 'Abgebrochen',
+  noFolderSelectedMessage: 'Es wurde kein Ordner ausgewählt.',
+  duplicateTitle: 'Hinweis',
+  duplicateFolderMessage: 'Dieser Ordner ist bereits in der Scan-Liste.',
+};
