c00c83dbdc86f1ebd7bf332836aacae7219e9335
k1w1-a0style <h91874350@gmail.com>
2026-05-16 06:37:51 +0200

    refactor: extract library import flow helpers

---
+++ utils/libraryImportFlow.ts
@@ -0,0 +1,27 @@
+import type { ScanFolder } from '../types/ScanFolder';
+import { libraryImportMessages } from './libraryImportMessages';
+
+interface LibraryAlertMessage {
+  title: string;
+  message: string;
+}
+
+export const shouldImportFromScanFolders = (activeFolders: ScanFolder[], platformOs: string): boolean =>
+  activeFolders.length > 0 && platformOs === 'android';
+
+export const hasImportErrors = (errors: readonly unknown[] | undefined): boolean =>
+  (errors?.length ?? 0) > 0;
+
+export const getEmptyScanImportAlert = (errors: readonly unknown[] | undefined): LibraryAlertMessage => {
+  if (hasImportErrors(errors)) {
+    return {
+      title: libraryImportMessages.scanFailedTitle,
+      message: libraryImportMessages.scanFailedMessage,
+    };
+  }
+
+  return {
+    title: libraryImportMessages.noMusicFoundTitle,
+    message: libraryImportMessages.noAudioInScanFoldersMessage,
+  };
+};
