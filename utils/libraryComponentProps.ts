aa21146d872e0663fc62f2c2c794bbb5a4fda7a3
k1w1-a0style <h91874350@gmail.com>
2026-05-18 00:26:11 +0200

    refactor: add library component props builders

---
+++ utils/libraryComponentProps.ts
@@ -0,0 +1,82 @@
+import type { LibraryMenuModalProps } from '../components/LibraryMenuModal';
+import type { LibraryTabContentProps } from '../components/LibraryTabContent';
+import type { Song } from '../types/Song';
+import type { ScanFolder } from '../types/ScanFolder';
+import type { LibraryTab } from './libraryTabs';
+import type { LibraryGroupItem } from './libraryPresentation';
+import type { LibraryPlaylistItem } from './libraryPlaylists';
+import type { LibraryAlbumViewMode } from '../components/LibraryAlbumViewToggle';
+
+interface LibraryTabContentPropsBuilderOptions {
+  activeTab: LibraryTab;
+  activeFolders: number;
+  albumGroups: LibraryGroupItem[];
+  albumViewMode: LibraryAlbumViewMode;
+  artistGroups: LibraryGroupItem[];
+  emptyMessage: string;
+  genreGroups: LibraryGroupItem[];
+  getSongItemLayout: LibraryTabContentProps['getSongItemLayout'];
+  onPlayActiveList: () => void;
+  onShuffle: () => void;
+  onToggleAlbumView: () => void;
+  playlistItems: LibraryPlaylistItem[];
+  renderAlbumTile: LibraryTabContentProps['renderAlbumTile'];
+  renderFolderItem: LibraryTabContentProps['renderFolderItem'];
+  renderGroupItem: LibraryTabContentProps['renderGroupItem'];
+  renderPlaylistItem: LibraryTabContentProps['renderPlaylistItem'];
+  renderSongItem: LibraryTabContentProps['renderSongItem'];
+  scanFolders: ScanFolder[];
+  songKeyExtractor: (item: Song) => string;
+  songsForActiveList: Song[];
+}
+
+interface LibraryMenuModalPropsBuilderOptions {
+  activeFolders: number;
+  closeMenu: () => void;
+  importFromDevice: () => void;
+  isReady: boolean;
+  loading: boolean;
+  menuOpen: boolean;
+  onAddScanFolder: () => void;
+  openSettings: () => void;
+  refreshMetadataFromFiles: () => void;
+  showScanFolders: () => void;
+  songsCount: number;
+}
+
+export const buildLibraryTabContentProps = (options: LibraryTabContentPropsBuilderOptions): LibraryTabContentProps => ({
+  activeTab: options.activeTab,
+  activeFolders: options.activeFolders,
+  albumGroups: options.albumGroups,
+  albumViewMode: options.albumViewMode,
+  artistGroups: options.artistGroups,
+  emptyMessage: options.emptyMessage,
+  genreGroups: options.genreGroups,
+  getSongItemLayout: options.getSongItemLayout,
+  onPlayActiveList: options.onPlayActiveList,
+  onShuffle: options.onShuffle,
+  onToggleAlbumView: options.onToggleAlbumView,
+  playlistItems: options.playlistItems,
+  renderAlbumTile: options.renderAlbumTile,
+  renderFolderItem: options.renderFolderItem,
+  renderGroupItem: options.renderGroupItem,
+  renderPlaylistItem: options.renderPlaylistItem,
+  renderSongItem: options.renderSongItem,
+  scanFolders: options.scanFolders,
+  songKeyExtractor: options.songKeyExtractor,
+  songsForActiveList: options.songsForActiveList,
+});
+
+export const buildLibraryMenuModalProps = (options: LibraryMenuModalPropsBuilderOptions): LibraryMenuModalProps => ({
+  visible: options.menuOpen,
+  loading: options.loading,
+  isReady: options.isReady,
+  hasSongs: options.songsCount > 0,
+  activeFolders: options.activeFolders,
+  onClose: options.closeMenu,
+  onImport: options.importFromDevice,
+  onRefreshMetadata: options.refreshMetadataFromFiles,
+  onAddFolder: options.onAddScanFolder,
+  onShowFolders: options.showScanFolders,
+  onOpenSettings: options.openSettings,
+});
