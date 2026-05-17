c01b2679850643e8a77ef6dc90b21d7351735eb4
k1w1-a0style <h91874350@gmail.com>
2026-05-17 23:20:55 +0200

    test: include library renderers barrel export

--- hooks/__tests__/libraryHooks.test.ts
+++ hooks/__tests__/libraryHooks.test.ts
@@ -1,17 +1,20 @@
 import {
   useLibraryImportActions,
   useLibraryMetadataRefreshActions,
+  useLibraryRenderers,
   useLibraryScanFolderActions,
   useLibraryStoredState,
 } from '../libraryHooks';
 import { useLibraryImportActions as directUseLibraryImportActions } from '../useLibraryImportActions';
 import { useLibraryMetadataRefreshActions as directUseLibraryMetadataRefreshActions } from '../useLibraryMetadataRefreshActions';
+import { useLibraryRenderers as directUseLibraryRenderers } from '../useLibraryRenderers';
 import { useLibraryScanFolderActions as directUseLibraryScanFolderActions } from '../useLibraryScanFolderActions';
 import { useLibraryStoredState as directUseLibraryStoredState } from '../useLibraryStoredState';
 
 test('exports library hooks', () => {
   expect(useLibraryImportActions).toBe(directUseLibraryImportActions);
   expect(useLibraryMetadataRefreshActions).toBe(directUseLibraryMetadataRefreshActions);
+  expect(useLibraryRenderers).toBe(directUseLibraryRenderers);
   expect(useLibraryScanFolderActions).toBe(directUseLibraryScanFolderActions);
   expect(useLibraryStoredState).toBe(directUseLibraryStoredState);
 });
