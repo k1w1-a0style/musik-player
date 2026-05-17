1d03960241c1399c6ebb6e00c1f837daf8eb52da
k1w1-a0style <h91874350@gmail.com>
2026-05-17 22:55:22 +0200

    refactor: export library renderers hook

--- hooks/libraryHooks.ts
+++ hooks/libraryHooks.ts
@@ -1,4 +1,5 @@
 export { useLibraryImportActions } from './useLibraryImportActions';
 export { useLibraryMetadataRefreshActions } from './useLibraryMetadataRefreshActions';
+export { useLibraryRenderers } from './useLibraryRenderers';
 export { useLibraryScanFolderActions } from './useLibraryScanFolderActions';
 export { useLibraryStoredState } from './useLibraryStoredState';
