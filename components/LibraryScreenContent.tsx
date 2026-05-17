cacfdb7bd3d73d29902cee0495e2abadc314aac0
k1w1-a0style <h91874350@gmail.com>
2026-05-18 01:30:36 +0200

    refactor: pass library tabs props to screen content

--- components/LibraryScreenContent.tsx
+++ components/LibraryScreenContent.tsx
@@ -4,39 +4,36 @@ import LibraryImportStatus from './LibraryImportStatus';
 import LibraryMenuModal, { type LibraryMenuModalProps } from './LibraryMenuModal';
 import LibrarySearchBar from './LibrarySearchBar';
 import LibraryTabContent, { type LibraryTabContentProps } from './LibraryTabContent';
-import LibraryTabs from './LibraryTabs';
+import LibraryTabs, { type LibraryTabsProps } from './LibraryTabs';
 import LibraryTopBar, { type LibraryTopBarProps } from './LibraryTopBar';
-import type { LibraryTab } from '../utils/libraryTabs';
 
 export interface LibraryScreenContentProps {
-  activeTab: LibraryTab;
   importStatus: string | null;
   loading: boolean;
   menuModalProps: LibraryMenuModalProps;
   query: string;
   searchOpen: boolean;
-  setActiveTab: Dispatch<SetStateAction<LibraryTab>>;
   setQuery: Dispatch<SetStateAction<string>>;
   tabContentProps: LibraryTabContentProps;
+  tabsProps: LibraryTabsProps;
   topBarProps: LibraryTopBarProps;
 }
 
 const LibraryScreenContent: React.FC<LibraryScreenContentProps> = ({
-  activeTab,
   importStatus,
   loading,
   menuModalProps,
   query,
   searchOpen,
-  setActiveTab,
   setQuery,
   tabContentProps,
+  tabsProps,
   topBarProps,
 }) => (
   <>
     <LibraryTopBar {...topBarProps} />
 
-    <LibraryTabs activeTab={activeTab} onChangeTab={setActiveTab} />
+    <LibraryTabs {...tabsProps} />
 
     {searchOpen && <LibrarySearchBar value={query} onChangeText={setQuery} autoFocus />}
     {loading && <LibraryImportStatus status={importStatus} />}
