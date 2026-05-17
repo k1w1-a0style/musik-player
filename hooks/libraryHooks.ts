82bf29ce4c479335246b4280655b17b64c9e0fcf
k1w1-a0style <h91874350@gmail.com>
2026-05-17 23:22:22 +0200

    refactor: export library playback actions hook

--- hooks/libraryHooks.ts
+++ hooks/libraryHooks.ts
@@ -1,5 +1,6 @@
 export { useLibraryImportActions } from './useLibraryImportActions';
 export { useLibraryMetadataRefreshActions } from './useLibraryMetadataRefreshActions';
+export { useLibraryPlaybackActions } from './useLibraryPlaybackActions';
 export { useLibraryRenderers } from './useLibraryRenderers';
 export { useLibraryScanFolderActions } from './useLibraryScanFolderActions';
 export { useLibraryStoredState } from './useLibraryStoredState';
