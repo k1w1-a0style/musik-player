d64215d0290ee18e8f2ece0dcc4243ad78079c33
k1w1-a0style <h91874350@gmail.com>
2026-05-18 01:05:44 +0200

    refactor: render library screen content component

--- screens/Library.tsx
+++ screens/Library.tsx
@@ -2,41 +2,16 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 import AppBackground from '../components/AppBackground';
 import Screen from '../components/Screen';
-import LibrarySearchBar from '../components/LibrarySearchBar';
-import LibraryTopBar from '../components/LibraryTopBar';
-import LibraryTabs from '../components/LibraryTabs';
-import LibraryImportStatus from '../components/LibraryImportStatus';
-import LibraryMenuModal from '../components/LibraryMenuModal';
-import LibraryTabContent from '../components/LibraryTabContent';
+import LibraryScreenContent from '../components/LibraryScreenContent';
 import { useLibraryController } from '../hooks/libraryHooks';
 
 const Library: React.FC = () => {
-  const {
-    activeTab,
-    importStatus,
-    loading,
-    menuModalProps,
-    query,
-    searchOpen,
-    setActiveTab,
-    setQuery,
-    tabContentProps,
-    topBarProps,
-  } = useLibraryController();
+  const controller = useLibraryController();
 
   return (
     <AppBackground>
       <Screen testID="library-screen" contentStyle={styles.container}>
-        <LibraryTopBar {...topBarProps} />
-
-        <LibraryTabs activeTab={activeTab} onChangeTab={setActiveTab} />
-
-        {searchOpen && <LibrarySearchBar value={query} onChangeText={setQuery} autoFocus />}
-        {loading && <LibraryImportStatus status={importStatus} />}
-
-        <LibraryTabContent {...tabContentProps} />
-
-        <LibraryMenuModal {...menuModalProps} />
+        <LibraryScreenContent {...controller} />
       </Screen>
     </AppBackground>
   );
