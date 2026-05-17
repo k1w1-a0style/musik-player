55fec27358d6cb5d3243afd3444bdf1ce6c7956c
k1w1-a0style <h91874350@gmail.com>
2026-05-17 23:42:56 +0200

    refactor: export library navigation actions hook

--- hooks/libraryHooks.ts
+++ hooks/libraryHooks.ts
@@ -1,6 +1,7 @@
 export { useLibraryImportActions } from './useLibraryImportActions';
 export { useLibraryMenuActions } from './useLibraryMenuActions';
 export { useLibraryMetadataRefreshActions } from './useLibraryMetadataRefreshActions';
+export { useLibraryNavigationActions } from './useLibraryNavigationActions';
 export { useLibraryPlaybackActions } from './useLibraryPlaybackActions';
 export { useLibraryRenderers } from './useLibraryRenderers';
 export { useLibraryScanFolderActions } from './useLibraryScanFolderActions';
