9679028da3885283a0322a48396032df2da126e4
k1w1-a0style <h91874350@gmail.com>
2026-05-18 00:49:14 +0200

    refactor: use library controller hook

--- screens/Library.tsx
+++ screens/Library.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { useLibraryMusicContext } from '../contexts/MusicContext';
 import AppBackground from '../components/AppBackground';
 import Screen from '../components/Screen';
 import LibrarySearchBar from '../components/LibrarySearchBar';
@@ -9,175 +8,26 @@ import LibraryTabs from '../components/LibraryTabs';
 import LibraryImportStatus from '../components/LibraryImportStatus';
 import LibraryMenuModal from '../components/LibraryMenuModal';
 import LibraryTabContent from '../components/LibraryTabContent';
-import {
-  useLibraryAlerts,
-  useLibraryComponentProps,
-  useLibraryImportActions,
-  useLibraryMenuActions,
-  useLibraryMetadataRefreshActions,
-  useLibraryNavigationActions,
-  useLibraryPlaybackActions,
-  useLibraryRenderers,
-  useLibraryScanFolderActions,
-  useLibraryScreenState,
-  useLibraryStoredState,
-  useLibraryViewState,
-} from '../hooks/libraryHooks';
+import { useLibraryController } from '../hooks/libraryHooks';
 
 const Library: React.FC = () => {
-  const { songs, setSongs, currentSong, playSong, isReady, isPlaying, playlists = [], playPlaylist = async () => undefined } = useLibraryMusicContext();
   const {
     activeTab,
-    albumViewMode,
     importStatus,
     loading,
-    menuOpen,
+    menuModalProps,
     query,
     searchOpen,
     setActiveTab,
-    setAlbumViewMode,
-    setImportStatus,
-    setLoading,
-    setMenuOpen,
     setQuery,
-    setSearchOpen,
-  } = useLibraryScreenState();
-  const { scanFolders, setScanFolders, favoriteIds } = useLibraryStoredState(activeTab);
-  const { openTrackInfo } = useLibraryNavigationActions();
-  const { showAlert } = useLibraryAlerts();
-
-  const currentSongId = currentSong?.id ?? null;
-  const {
-    activeFolders,
-    albumGroups,
-    artistGroups,
-    emptyMessage,
-    filteredSongs,
-    genreGroups,
-    playlistItems,
-    songsForActiveList,
-  } = useLibraryViewState({
-    activeTab,
-    favoriteIds,
-    isReady,
-    playlists,
-    query,
-    scanFolders,
-    songs,
-  });
-
-  const {
-    closeMenu,
-    openMenu,
-    openSettings,
-    toggleSearch,
-  } = useLibraryMenuActions({
-    setMenuOpen,
-    setSearchOpen,
-    showAlert,
-  });
-
-  const {
-    showScanFolders,
-    onAddScanFolder,
-    persistChangedFolderUpdates,
-    removeFolder,
-  } = useLibraryScanFolderActions({
-    scanFolders,
-    setScanFolders,
-    setActiveTab,
-    setMenuOpen,
-    showAlert,
-  });
-
-  const { importFromDevice } = useLibraryImportActions({
-    scanFolders,
-    songs,
-    setSongs,
-    setActiveTab,
-    setMenuOpen,
-    setLoading,
-    setImportStatus,
-    showAlert,
-    persistChangedFolderUpdates,
-  });
-
-  const { refreshMetadataFromFiles } = useLibraryMetadataRefreshActions({
-    songs,
-    setSongs,
-    setMenuOpen,
-    setLoading,
-    setImportStatus,
-    showAlert,
-  });
-
-  const {
-    getSongItemLayout,
-    handleSongPress,
-    renderAlbumTile,
-    renderFolderItem,
-    renderGroupItem,
-    renderPlaylistItem,
-    renderSongItem,
-    songKeyExtractor,
-  } = useLibraryRenderers({
-    currentSongId,
-    filteredSongs,
-    isPlaying,
-    onOpenTrackInfo: openTrackInfo,
-    playPlaylist,
-    playSong,
-    removeFolder,
-  });
-
-  const {
-    handlePlayActiveList,
-    handleShufflePress,
-    toggleAlbumView,
-  } = useLibraryPlaybackActions({
-    handleSongPress,
-    playSong,
-    setAlbumViewMode,
-    songsForActiveList,
-  });
-
-  const { menuModalProps, tabContentProps } = useLibraryComponentProps({
-    activeFolders,
-    activeTab,
-    albumGroups,
-    albumViewMode,
-    artistGroups,
-    closeMenu,
-    emptyMessage,
-    genreGroups,
-    getSongItemLayout,
-    handlePlayActiveList,
-    handleShufflePress,
-    importFromDevice,
-    isReady,
-    loading,
-    menuOpen,
-    onAddScanFolder,
-    openSettings,
-    playlistItems,
-    refreshMetadataFromFiles,
-    renderAlbumTile,
-    renderFolderItem,
-    renderGroupItem,
-    renderPlaylistItem,
-    renderSongItem,
-    scanFolders,
-    showScanFolders,
-    songKeyExtractor,
-    songsCount: songs.length,
-    songsForActiveList,
-    toggleAlbumView,
-  });
+    tabContentProps,
+    topBarProps,
+  } = useLibraryController();
 
   return (
     <AppBackground>
       <Screen testID="library-screen" contentStyle={styles.container}>
-        <LibraryTopBar onToggleSearch={toggleSearch} onOpenMenu={openMenu} />
+        <LibraryTopBar {...topBarProps} />
 
         <LibraryTabs activeTab={activeTab} onChangeTab={setActiveTab} />
 
