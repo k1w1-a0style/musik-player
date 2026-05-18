8818e8f87580088505e07af0e20a1adf30c7582c
k1w1-a0style <h91874350@gmail.com>
2026-05-18 14:32:51 +0200

    Test audio playback mode helpers

---
+++ utils/__tests__/audioPlaybackModes.test.ts
@@ -0,0 +1,10 @@
+import { RepeatMode as RNTPRepeatMode } from 'react-native-track-player';
+import { toTrackPlayerRepeatMode } from '../audioPlaybackModes';
+
+describe('audioPlaybackModes', () => {
+  test('maps app repeat modes to TrackPlayer repeat modes', () => {
+    expect(toTrackPlayerRepeatMode('off')).toBe(RNTPRepeatMode.Off);
+    expect(toTrackPlayerRepeatMode('one')).toBe(RNTPRepeatMode.Track);
+    expect(toTrackPlayerRepeatMode('all')).toBe(RNTPRepeatMode.Queue);
+  });
+});
