8f032c34d689cb8e61ef559f1135787494b03aaf
k1w1-a0style <h91874350@gmail.com>
2026-05-19 04:41:40 +0200

    Extract app font map

---
+++ appFonts.ts
@@ -0,0 +1,13 @@
+import {
+  BricolageGrotesque_400Regular,
+  BricolageGrotesque_500Medium,
+  BricolageGrotesque_600SemiBold,
+  BricolageGrotesque_700Bold,
+} from '@expo-google-fonts/bricolage-grotesque';
+
+export const appFonts = {
+  'Bricolage-Regular': BricolageGrotesque_400Regular,
+  'Bricolage-Medium': BricolageGrotesque_500Medium,
+  'Bricolage-SemiBold': BricolageGrotesque_600SemiBold,
+  'Bricolage-Bold': BricolageGrotesque_700Bold,
+};
