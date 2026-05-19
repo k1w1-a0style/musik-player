3f287f353a0aeb2e1e5a2bedab9d574a57c4542b
k1w1-a0style <h91874350@gmail.com>
2026-05-19 15:46:59 +0200

    Handle remote playback promise errors

--- services/PlaybackService.ts
+++ services/PlaybackService.ts
@@ -6,13 +6,13 @@ import TrackPlayer, { Event } from 'react-native-track-player';
  */
 export const PlaybackService = async (): Promise<void> => {
   TrackPlayer.addEventListener(Event.RemotePlay, () => {
-    TrackPlayer.play();
+    TrackPlayer.play().catch(() => undefined);
   });
   TrackPlayer.addEventListener(Event.RemotePause, () => {
-    TrackPlayer.pause();
+    TrackPlayer.pause().catch(() => undefined);
   });
   TrackPlayer.addEventListener(Event.RemoteStop, () => {
-    TrackPlayer.stop();
+    TrackPlayer.stop().catch(() => undefined);
   });
   TrackPlayer.addEventListener(Event.RemoteNext, () => {
     TrackPlayer.skipToNext().catch(() => undefined);
@@ -21,12 +21,12 @@ export const PlaybackService = async (): Promise<void> => {
     TrackPlayer.skipToPrevious().catch(() => undefined);
   });
   TrackPlayer.addEventListener(Event.RemoteSeek, ({ position }) => {
-    TrackPlayer.seekTo(position);
+    TrackPlayer.seekTo(position).catch(() => undefined);
   });
   TrackPlayer.addEventListener(Event.RemoteJumpForward, ({ interval }) => {
-    TrackPlayer.seekBy(interval ?? 10);
+    TrackPlayer.seekBy(interval ?? 10).catch(() => undefined);
   });
   TrackPlayer.addEventListener(Event.RemoteJumpBackward, ({ interval }) => {
-    TrackPlayer.seekBy(-(interval ?? 10));
+    TrackPlayer.seekBy(-(interval ?? 10)).catch(() => undefined);
   });
 };
