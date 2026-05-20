70f17e2b9529fe8fa758d00c8570e212351eb9e5
k1w1-a0style <h91874350@gmail.com>
2026-05-20 06:42:38 +0200

    Cover queue playlist provider input

--- contexts/__tests__/musicProviderActionInput.test.ts
+++ contexts/__tests__/musicProviderActionInput.test.ts
@@ -20,6 +20,7 @@ describe('musicProviderActionInput', () => {
   test('builds context playlist action input', () => {
     const actions = {
       createPlaylist: () => ({ id: 'pl-1', name: 'List', songIds: [], createdAt: 1 }),
+      saveQueueAsPlaylist: () => ({ id: 'pl-2', name: 'Queue', songIds: ['s1'], createdAt: 2 }),
       deletePlaylist: noop,
       renamePlaylist: noop,
       addSongToPlaylist: noop,
