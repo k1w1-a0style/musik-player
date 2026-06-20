58b5c6f9d3c9ae00abe1469614212e0597763a1e
k1w1-a0style <h91874350@gmail.com>
2026-06-20 18:32:07 +0200

    Include album artist in tag write intent detection

--- utils/tagWriterDraft.ts
+++ utils/tagWriterDraft.ts
@@ -1,5 +1,17 @@
 import type { TagEditDraft } from '../types/TagEdit';
 
+export const EDITABLE_TAG_KEYS: Array<keyof TagEditDraft['tags']> = [
+  'title',
+  'artist',
+  'albumArtist',
+  'album',
+  'year',
+  'genre',
+  'trackNumber',
+  'discNumber',
+  'comment',
+];
+
 export const hasDraftTagIntent = (
   draft: TagEditDraft,
   key: keyof TagEditDraft['tags'],
@@ -7,16 +19,6 @@ export const hasDraftTagIntent = (
   Object.prototype.hasOwnProperty.call(draft.tags, key) && draft.tags[key] !== undefined;
 
 export const hasAnyTagEditIntent = (draft: TagEditDraft): boolean => {
-  const keys: Array<keyof TagEditDraft['tags']> = [
-    'title',
-    'artist',
-    'album',
-    'year',
-    'genre',
-    'trackNumber',
-    'discNumber',
-    'comment',
-  ];
-  const hasTagIntent = keys.some(key => hasDraftTagIntent(draft, key));
+  const hasTagIntent = EDITABLE_TAG_KEYS.some(key => hasDraftTagIntent(draft, key));
   return hasTagIntent || Boolean(draft.cover) || draft.removeCover === true;
 };
