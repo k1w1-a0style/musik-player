199412b55fc2a1a5b738ee21d720c3eeaa557536
k1w1-a0style <h91874350@gmail.com>
2026-06-21 21:24:01 +0200

    Pass current song to waveform playback section

--- screens/NowPlayingPlayerPanel.tsx
+++ screens/NowPlayingPlayerPanel.tsx
@@ -50,7 +50,7 @@ const NowPlayingPlayerPanel: React.FC<NowPlayingPlayerPanelProps> = ({
   onOpenTrackInfo,
 }) => (
   <View style={styles.playerPage} testID="now-playing-player-panel">
-    <View style={[styles.coverArea, { height: coverAreaHeight }]}>
+    <View style={[styles.coverArea, { height: coverAreaHeight }]}> 
       <NowPlayingCoverArtwork
         song={currentSong}
         artworkUri={artworkUri}
@@ -68,6 +68,7 @@ const NowPlayingPlayerPanel: React.FC<NowPlayingPlayerPanelProps> = ({
     />
 
     <NowPlayingPlaybackSection
+      currentSong={currentSong}
       position={position}
       duration={duration}
       onSeek={onSeek}
