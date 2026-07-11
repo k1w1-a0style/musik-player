78fcb16f767cb07a6a1f7bddbc6c8ce02a29247e
k1w1-a0style <h91874350@gmail.com>
2026-07-11 08:26:45 +0200

    Restore sleep timer start menu entry

--- screens/NowPlayingMenuModal.tsx
+++ screens/NowPlayingMenuModal.tsx
@@ -61,6 +61,13 @@ const NowPlayingMenuModal: React.FC<NowPlayingMenuModalProps> = ({
               onClose();
             }}
           />
+          <NowPlayingMenuItem
+            label="Sleep-Timer starten"
+            onPress={() => {
+              onStartSleepTimer(30);
+              onClose();
+            }}
+          />
 
           {[15, 30, 45, 60].map(minutes => (
             <NowPlayingMenuItem
